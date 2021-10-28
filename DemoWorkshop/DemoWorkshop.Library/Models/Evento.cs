0fba6b82adb67bdb543ca20ff4c239c89666546e
Salvatore Sorrentino <salvatore.sorrentino@live.com>
2021-10-28 10:16:38 +0200

    demo workshop

---
+++ DemoWorkshop/DemoWorkshop.Library/Models/Evento.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoWorkshop.Library.Models
+{
+    public class Evento
+    {
+        public int Id { get; set; }
+        public string Titolo { get; set; }
+        public DateTime Data { get; set; }
+        public string Localita { get; set; }
+    }
+}
