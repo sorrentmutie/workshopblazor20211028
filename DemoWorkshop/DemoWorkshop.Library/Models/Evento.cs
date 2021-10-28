bfdd16ade0c8ecfff8834e98b18fd9a67eeffd73
Salvatore Sorrentino <salvatore.sorrentino@live.com>
2021-10-28 14:53:45 +0200

    componenti generici

--- DemoWorkshop/DemoWorkshop.Library/Models/Evento.cs
+++ DemoWorkshop/DemoWorkshop.Library/Models/Evento.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,9 +9,14 @@ namespace DemoWorkshop.Library.Models
 {
     public class Evento
     {
+        [Required]
         public int Id { get; set; }
+        [Required]
         public string Titolo { get; set; }
+        [Required]
+
         public DateTime Data { get; set; }
+        [Required]
         public string Localita { get; set; }
     }
 }
